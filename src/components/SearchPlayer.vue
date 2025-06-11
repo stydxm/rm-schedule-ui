f2bb27c5b1ec9173567016b021d89c94cd1d5f74
stydxm <stydxm@outlook.com>
2025-06-12 03:35:35 +0800

    feat: auto redirect to suitable zone when searching for a team

--- src/components/SearchPlayer.vue
+++ src/components/SearchPlayer.vue
@@ -21,9 +21,10 @@ const promotionStore = usePromotionStore()
 
 const onlyCurrentZone = ref(false)
 const selected = ref<Player>()
+let zones
 const players = computed(() => {
   let ret: Player[] = []
-  let zones = promotionStore.schedule.data.event.zones.nodes
+  zones = promotionStore.schedule.data.event.zones.nodes
   if (onlyCurrentZone.value) {
     zones = zones.filter(zone => zone.id == props.zoneId.toString())
   }
@@ -43,14 +44,38 @@ function customFilter(value: string, query: string, item?: InternalItem<Player>)
     PinyinMatch.match(t.collegeName, query) || PinyinMatch.match(t.name, query)
 }
 
+function handleZoneChange() {
+  // 当选中队伍不在当前赛区时，自动跳转到该队伍参赛且编号最大的赛区
+  let inCurrentZone = false
+  const currenZone = promotionStore.getZone(props.zoneId)
+  for (let group of currenZone.groups.nodes) {
+    if (group.players.nodes.some(p => p.id == promotionStore.selectedPlayer.id)) {
+      inCurrentZone = true
+      break
+    }
+  }
+  if (!inCurrentZone) {
+    let latestZoneId = props.zoneId
+    for (let zone of zones) {
+      for (let group of zone.groups.nodes) {
+        if (group.players.nodes.some(p => p.id == promotionStore.selectedPlayer.id)) {
+          latestZoneId = zone.id
+          // 此处故意不写break，目的是当一个队伍参加多个赛区（区域赛+国赛）时，取最晚的组
+        }
+      }
+    }
+    if (latestZoneId != props.zoneId) promotionStore.zoneId = latestZoneId
+  }
+}
+
 function confirm() {
   appStore.searchDialog = false
   promotionStore.selectedPlayer = selected.value
+  if (!onlyCurrentZone.value) handleZoneChange()
 }
 
 function analyze() {
-  appStore.searchDialog = false
-  promotionStore.selectedPlayer = selected.value
+  confirm()
   appStore.analysisDialog = true
 }
 </script>
