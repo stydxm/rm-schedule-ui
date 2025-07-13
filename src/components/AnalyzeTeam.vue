3c3009848767f16a7916051aae024a8ca058176d
Summer-lights <1067088037@qq.com>
2025-07-13 16:29:41 +0800

    refactor: add robot data mapping and display in AnalyzeTeam.vue

--- src/components/AnalyzeTeam.vue
+++ src/components/AnalyzeTeam.vue
@@ -75,6 +75,82 @@ function convertToOrdinal(number: number): string {
     return number + "th";
   }
 }
+
+const robotData = computed(() => {
+  return promotionStore.robotData.zones.find((zone) => {
+    return Number(zone.zoneId) == props.zoneId
+  }).teams.find((team) => {
+    return team.collegeName == props.player.team.collegeName
+  })
+})
+
+const RobotDataMap = ref({
+  "Hero": {
+    type: "英雄",
+    dataFields: [
+      { th: "局均42mm弹丸命中率(%)", td: "eaBigHitRate", },
+      { th: "局均部署命中数", td: "eaSnipeCnt", },
+      { th: "局均伤害", td: "gkDamage", },
+      { th: "局均KDA", td: "eaKDA", },
+    ]
+  },
+  "Infantry": {
+    type: "步兵",
+    dataFields: [
+      { th: "局均17mm弹丸命中率(%)", td: "eaSmallHitRate", },
+      { th: "局均总伤害", td: "eagHurt", },
+      { th: "局均关键伤害", td: "gkDamage", },
+      { th: "局均KDA", td: "eaKDA", },
+    ]
+  },
+  "Sapper": {
+    type: "工程",
+    dataFields: [
+      { th: "局均兑换难度", td: "avgMineDiff", },
+      { th: "局均兑换时间", td: "avgMineTime", },
+      { th: "局均兑换经济", td: "eaExchangeEcon", },
+      { th: "局均KDA", td: "eaKDA", },
+    ]
+  },
+  "Airplane": {
+    type: "无人机",
+    dataFields: [
+      { th: "局均17mm弹丸命中率(%)", td: "eaSmallHitRate", },
+      { th: "局均发弹量", td: "avgShootNum", },
+      { th: "局均总伤害", td: "eagHurt", },
+      { th: "局均关键伤害", td: "gkDamage", },
+      { th: "局均KDA", td: "eaKDA", },
+    ]
+  },
+  "Guard": {
+    type: "哨兵",
+    dataFields: [
+      { th: "局均17mm弹丸命中率(%)", td: "eaSmallHitRate", },
+      { th: "局均总伤害", td: "eagHurt", },
+      { th: "局均关键伤害", td: "gkDamage", },
+      { th: "局均KDA", td: "eaKDA", },
+    ]
+  },
+  "Dart": {
+    type: "飞镖",
+    dataFields: [
+      { th: "累计命中前哨站数", td: "etDartOutpostCnt", },
+      { th: "累计命中基地固定目标数", td: "etDartFixedCnt", },
+      { th: "累计命中基地随机固定目标数", td: "etDartRDFixCnt", },
+      { th: "累计命中基地随机移动目标数", td: "etDartRDMoveCnt", },
+      { th: "局均KDA", td: "eaKDA", },
+    ]
+  },
+  "Radar": {
+    type: "雷达",
+    dataFields: [
+      { th: "局均额外伤害", td: "eaRadarDebuffDmg", },
+      { th: "局均易伤时间", td: "eaRadarMarkerTime", },
+    ]
+  }
+})
+
+console.log(robotData.value)
 </script>
 
 <template>
@@ -110,6 +186,43 @@ function convertToOrdinal(number: number): string {
       <div v-else>
         <div v-if="rank">
           <v-row>
+            <v-col md="6" cols="12">
+              <div>
+                <v-chip class="mb-2" color="info" variant="flat" label>
+                  <h3>机器人数据</h3>
+                </v-chip>
+
+                <div v-for="robot in robotData.robots"
+                     :key="robot.robotNumber">
+                  <div v-if="RobotDataMap[robot.type]" class="mt-2">
+                    <v-chip color="info" variant="tonal" label size="small">
+                      <h3>{{ RobotDataMap[robot.type].type }}</h3>
+                    </v-chip>
+
+                    <v-table density="compact">
+                      <thead>
+                      <tr>
+                        <th v-for="field in RobotDataMap[robot.type].dataFields"
+                            :key="field.th"
+                            class="text-left">
+                          <span>{{ field.th }}</span>
+                        </th>
+                      </tr>
+                      </thead>
+                      <tbody>
+                      <tr>
+                        <td v-for="field in RobotDataMap[robot.type].dataFields"
+                            :key="field.td">
+                          <span>{{ robot[field.td] }}</span>
+                        </td>
+                      </tr>
+                      </tbody>
+                    </v-table>
+                  </div>
+                </div>
+              </div>
+            </v-col>
+
             <v-col md="6" cols="12">
               <div>
                 <v-chip color="info" variant="flat" label>
@@ -179,12 +292,16 @@ function convertToOrdinal(number: number): string {
                   </tr>
                   <tr>
                     <td>初始金币-项目文档</td>
-                    <td v-if="promotionStore.season!==2024">{{ rank.completeForm.initialCoinDocument }} ({{ rank.completeForm.levelDocument }})</td>
+                    <td v-if="promotionStore.season!==2024">{{ rank.completeForm.initialCoinDocument }}
+                      ({{ rank.completeForm.levelDocument }})
+                    </td>
                     <td v-else>{{ rank.completeForm.initialCoinDocument }}</td>
                   </tr>
                   <tr>
                     <td>初始金币-技术方案</td>
-                    <td v-if="promotionStore.season!==2024">{{ rank.completeForm.initialCoinTechnology }} ({{ rank.completeForm.levelTechnology }})</td>
+                    <td v-if="promotionStore.season!==2024">{{ rank.completeForm.initialCoinTechnology }}
+                      ({{ rank.completeForm.levelTechnology }})
+                    </td>
                     <td v-else>{{ rank.completeForm.initialCoinTechnology }}</td>
                   </tr>
                   <tr>
