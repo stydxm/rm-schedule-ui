5c2a65e4f4c6778644a4f96d291396d460177327
stydxm <stydxm@outlook.com>
2025-07-23 01:38:16 +0800

    fix: fetch logo with cdn
    Signed-off-by: stydxm <stydxm@outlook.com>

--- src/components/TeamHeader.vue
+++ src/components/TeamHeader.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { Player } from "../types/schedule";
 import { RankListItem } from "../types/rank";
+import logoCDN from './MatchGraph.vue'
 
 interface Props {
   player: Player,
@@ -16,7 +17,7 @@ const props = defineProps<Props>()
     <div class="left-column">
       <v-avatar size="100">
         <v-img
-          :src="props.player.team.collegeLogo"
+          :src="logoCDN(props.player.team.collegeLogo)"
           color="white"
         ></v-img>
       </v-avatar>
